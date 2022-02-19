e3bf36f9f3c89c886faed5a9394e4c4c3050dead
Omar <sliding_xp@protonmail.com>
2022-02-19 17:25:36 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import PrimeVue from 'primevue/config';
+import router from './router';
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue, { ripple: true });
+app.mount('#app');
